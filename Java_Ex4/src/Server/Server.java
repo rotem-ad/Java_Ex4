094c4c94e9678ff219d941f0dd1088323818dde9
adhohro <adhohro@41-12.cs.colman.ac.il>
2014-05-13 16:30:27 +0300

    Create Server class

---
+++ Java_Ex4/src/Server/Server.java
@@ -0,0 +1,34 @@
+package Server;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public Server() {
+		
+	}
+	
+	
+	private void initServer() {
+		//ServerSocket server = new ServerSocket(12345);
+		
+		//server.s
+		
+		 /*Socket someClient = server.accept();
+		ObjectOutputStream output=new ObjectOutputStream(someClient.getOutputStream());
+		 ObjectInputStream input=new ObjectInputStream(someClient.getInputStream());
+		output.writeObject("you are connected to the server");
+		 output.flush();
+		 String messageIn=(String)input.readObject();
+		 System.out.println("message from the client: "+messageIn);
+		 output.writeObject("bye");
+		 output.flush();
+
+		 output.close();
+		 input.close();
+		 someClient.close();*/
+	}
+}
