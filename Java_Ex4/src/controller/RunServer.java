d5bb3e247244e377188662223d4961b2d5335255
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-05-17 22:58:43 +0300

    Add loading server settings from XML file

--- Java_Ex4/src/controller/RunServer.java
+++ Java_Ex4/src/controller/RunServer.java
@@ -1,15 +1,72 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import model.ClientHandler;
-import model.ConnectionHandler;
 import model.Server;
+
+import com.thoughtworks.xstream.XStream;
+
+import config.ServerConf;
 
 public class RunServer {
 
 	public static void main(String[] args) {
-		ClientHandler ch = new ConnectionHandler(); 
 		
-		Server srv = new Server(5000, 3, ch);
+		/*XStream xs = new XStream();
+		ServerConf srvConf = new ServerConf(5000, 8, new ConnectionHandler());
+
+		String xmlString = xs.toXML(srvConf);
+		
+		BufferedWriter xmlOut = null;
+		try {
+			xmlOut = new BufferedWriter(new FileWriter("resources/ConfigFiles/serverConf.xml"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			xmlOut.write(xmlString);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		// Close xml file
+		try {
+			xmlOut.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+		
+		
+		XStream xs = new XStream();
+		
+		BufferedReader xmlInput = null;
+		try {
+			xmlInput = new BufferedReader(new FileReader("resources/ConfigFiles/serverConf.xml"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		// Load server configuration settings from XML file
+		ServerConf srvConfig = (ServerConf) xs.fromXML(xmlInput);
+		
+		// Close xml file
+		try {
+			xmlInput.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		ClientHandler handler = srvConfig.getHandler();
+		int port = srvConfig.getPort();
+		int poolSize = srvConfig.getPoolSize();
+		
+		Server srv = new Server(port,poolSize,handler);
 	
 		new Thread(srv).start();
 
