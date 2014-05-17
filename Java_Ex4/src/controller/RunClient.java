6511adfd0313dbaa05abf6bc8125782fa1236269
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-05-17 21:04:19 +0300

    Add object support

--- Java_Ex4/src/controller/RunClient.java
+++ Java_Ex4/src/controller/RunClient.java
@@ -2,8 +2,7 @@ package controller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
+import java.io.ObjectOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
@@ -12,22 +11,23 @@ public class RunClient {
 	public static void main(String[] srgs) throws Exception 
 	{
 		Socket  myServer = new Socket(InetAddress.getLocalHost(), 5000);
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(myServer.getInputStream()));
-		PrintWriter outToServer = new PrintWriter(new OutputStreamWriter(myServer.getOutputStream()));
+		
+		//ObjectInputStream inFromServer = new ObjectInputStream(myServer.getInputStream()); // This line caused a strange BUG!
+		ObjectOutputStream outToServer = new ObjectOutputStream(myServer.getOutputStream());
 		
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		String line;
 		//read input
-		while(!(line = inFromUser.readLine()).equals("exit"))
+		while(!(line=inFromUser.readLine()).equals("exit"))
 		{
-			outToServer.println(line); //notify server
+			outToServer.writeObject(line); // Send object to server output stream
 			outToServer.flush();
 		}
-		outToServer.println(line);
+		outToServer.writeObject(line);
+		outToServer.flush();
 		
 		//close all stuff
 		inFromUser.close();
-		inFromServer.close();
 		outToServer.close();
 		myServer.close();
 	}
