b8e42f7d70eac8ab261e5d7af4c6e4cdec6801f1
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-05-17 22:57:41 +0300

    server conf

--- Java_Ex4/src/config/ServerConf.java
+++ Java_Ex4/src/config/ServerConf.java
@@ -1,5 +1,30 @@
 package config;
 
-public class ServerConf {
+import model.ClientHandler;
 
+public class ServerConf {
+	// Data Members
+	private int port;
+	private int poolSize;
+	private ClientHandler handler;
+	
+	// ServerConf constructor
+	public ServerConf(int port, int poolSize, ClientHandler handler){
+		this.port = port;
+		this.poolSize = poolSize;
+		this.handler = handler;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public int getPoolSize() {
+		return poolSize;
+	}
+
+	public ClientHandler getHandler() {
+		return handler;
+	}
+	
 }
