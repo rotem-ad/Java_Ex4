edad39d0a6ba849d375164245d6796d192967f4d
Rotem Adhoh <rotem.adhoh@gmail.com>
2014-05-17 21:40:02 +0300

    bug fix

--- Java_Ex4/src/model/Server.java
+++ Java_Ex4/src/model/Server.java
@@ -1,34 +1,102 @@
 package model;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server {
-
-	public Server() {
-		
+public class Server implements Runnable {
+	// Data Members
+	ServerSocket server;
+	private int port;
+	private int poolSize;
+	private ClientHandler handler;
+	private boolean stop;
+	
+	// Methods
+	
+	// Server constructor
+	public Server(int port, int poolSize, ClientHandler handler) {
+		this.port = port;
+		this.poolSize = poolSize;
+		this.handler = handler;
+		this.stop = false;
 	}
 	
-	
-	private void initServer() {
-		//ServerSocket server = new ServerSocket(12345);
-		
-		//server.s
-		
-		 /*Socket someClient = server.accept();
-		ObjectOutputStream output=new ObjectOutputStream(someClient.getOutputStream());
-		 ObjectInputStream input=new ObjectInputStream(someClient.getInputStream());
-		output.writeObject("you are connected to the server");
-		 output.flush();
-		 String messageIn=(String)input.readObject();
-		 System.out.println("message from the client: "+messageIn);
-		 output.writeObject("bye");
-		 output.flush();
+	@Override
+	public void run() {
+		try {
+			server = new ServerSocket(port);
+			System.out.println("Server started");
+			// Server timeout
+			server.setSoTimeout(1000);
+			
+			// Thread pool creation 
+			ExecutorService threadPool = Executors.newFixedThreadPool(poolSize);
+			
+			// Server main loop
+			while (!stop) {
+				try {
+				//System.out.println("Waiting for client");
+				final Socket client = server.accept();
+				System.out.println("Client connected");
+				
+				/*final BufferedReader inFromClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				final PrintWriter out2Client = new PrintWriter(new OutputStreamWriter(client.getOutputStream())); */
+				
+				final ObjectInputStream inFromClient = new ObjectInputStream(client.getInputStream());
+				final ObjectOutputStream out2Client = new ObjectOutputStream(client.getOutputStream());
+				
+				threadPool.execute(new Runnable() {
+					
+					@Override
+					public void run() {
+						// Handle the client's request
+						handler.handleClient(inFromClient, out2Client);
+						try {
+							inFromClient.close();
+							out2Client.close();
+							client.close();
+							
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+						
+					}
+				});
 
-		 output.close();
-		 input.close();
-		 someClient.close();*/
+				} catch (SocketTimeoutException e) {
+					//System.out.println("Connection timed out");
+				}
+			}
+			
+			// When stop is TRUE, terminate the server
+			threadPool.shutdownNow();
+			server.close();
+			System.out.println("Server terminated");
+			
+		} catch (IOException e) {
+			// For example: connection unexpectedly disconnected
+			e.printStackTrace();
+		}
+
 	}
+	
+	public void stopServer() {
+		// Setting stop flag to TRUE
+		this.stop = true;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public int getPoolSize() {
+		return poolSize;
+	}	
+
 }
