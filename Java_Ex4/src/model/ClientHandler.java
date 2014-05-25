93de46f921a6fb7e1316f377e101bb65a7e9adf1
ROTEM-A <ROTEM-A@rotema.yedioth.co.il>
2014-05-25 16:15:18 +0300

    Add javadoc

--- Java_Ex4/src/model/ClientHandler.java
+++ Java_Ex4/src/model/ClientHandler.java
@@ -3,10 +3,17 @@ package model;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import model.algorithm.Solver;
-
+/**
+ * Interface for handling different types of clients (2048, maze game, etc. )
+ */
 public interface ClientHandler {
 	
+	/**
+	 * @author ROTEM-A
+	 * 
+	 * @param inFromClient client's object input stream
+	 * @param out2Client client's object output stream
+	 */
 	public void handleClient(ObjectInputStream inFromClient, ObjectOutputStream out2Client);
 
 }
